73112c131ebdc2a9c19794aaaabbb9c9d0810c9a
mccoleman75225 <michaelcoleman19@gmail.com>
2018-09-15 07:02:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function printBadges(array){
+  for (var i = 0; i < array.length; i++){
+    var year = i + 1
+    console.log("Welcome " + array[i] + "! You are employee #" + year + ".")
+  }
+  return array
+}
+
+function tailsNeverFails () {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
